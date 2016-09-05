41c42cc447310bda6e5cd8b6a95bd1029df7e7d5
Chicloon <Chicloon@gmail.com>
2016-09-05 22:55:58 +0900

    still working

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -2,11 +2,13 @@ import React, { Component, PropTypes } from 'react';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
-    
     render() {
+        console.log(this.test);        
         console.log(this.props.task);
+        let taskObj = this.props.task;
+        taskObj._id = 'bla bla';
         return (
-            <li>{this.props.task.text}</li>            
+            <li>{this.props.task.text} <p> {taskObj._id} </p> </li>            
         );
     }    
 }
