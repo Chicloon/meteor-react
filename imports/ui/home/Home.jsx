b6edd9fc525b90dae8f25e0d403fe67564e9a979
Chicloon <Chicloon@gmail.com>
2016-10-14 13:18:31 +0900

    animations makeup

--- imports/ui/home/Home.jsx
+++ imports/ui/home/Home.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { Session } from 'meteor/session';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 export default class Home extends Component {
-    
+
     render() {
-        Session.set('Meteor.loginButtons.dropdownVisible',false); 
+        Session.set('Meteor.loginButtons.dropdownVisible', false);
         return (
             <div className="container">
-                <p> Wecmole to Home, my dear! </p>
+                <ReactCSSTransitionGroup
+                    component='div'
+                    transitionName='example'
+                    transitionEnterTimeout={600}
+                    transitionAppearTimeout={600}
+                    transitionLeaveTimeout={400}
+                    transitionAppear={true}
+                >
+                <p > Wecmole to Home, my dear! </p>
                 <p> To login as administrator <br/> login: admin <br/> password: admin </p>
+                </ReactCSSTransitionGroup>
+                
+
+
             </div>
+
         );
     }
 
